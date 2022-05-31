a81d47d88a69981e718fb0a3f503afa2ca1b0bbd
Welton Lopes <weltonlopesmetal@gmail.com>
2022-05-31 16:47:54 -0300

    feat: create users  model

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,13 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role:DataTypes.STRING,
+  },{ tableName: 'users', timestamps: false }
+  
+  );
+  
+  return User;}
+
+module.exports = User;
