c22d6f5a459134deec871e31053933fe0c70d7bb
Valdeci97 <moura97.valdeci@gmail.com>
2022-06-01 15:34:11 -0300

    NEW: Create login controller

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -4,9 +4,6 @@ const { User } = require('../database/models');
 const login = async (email, password) => {
   try {
     const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return { message: 'User does not exist' };
-    }
     if (md5(password) !== user.password) {
       return { message: 'Incorrect email or password' };
     }
