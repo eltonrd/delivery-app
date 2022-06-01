507816f489bd3f5171d87f736fe63c04903f04cc
Braien Machado de Paula <braienmp@outlook.com>
2022-06-01 19:11:03 -0300

    fix: use cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const loginRouter = require('../routes/login');
 const errorMidleware = require('../middlewares/error');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use(errorMidleware);
