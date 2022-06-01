7bee2da12d78623ddd89be659fedcb94fc461270
Valdeci97 <moura97.valdeci@gmail.com>
2022-06-01 16:37:07 -0300

    NEW: Create and add error midlleware on server

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const loginRouter = require('../routes/login');
+const errorMidleware = require('../middlewares/error');
 
 const app = express();
 app.use(express.json());
 app.use('/login', loginRouter);
+app.use(errorMidleware);
 
 module.exports = app;
