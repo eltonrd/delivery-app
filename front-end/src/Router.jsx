77e1c372534efd6592ee591b829b5974d8dae354
Braien Machado de Paula <braienmp@outlook.com>
2022-06-01 16:11:02 -0300

    fix: root path redirects to /login

--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" replace /> } />
+      <Route path="/login" element={ <Login /> } />
     </Routes>
   );
 }
