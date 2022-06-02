1d48ada344d06b2eb4deb2512c48d18d1ce1332b
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-02 19:29:38 -0300

    FEAT: admin route implementation

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const loginRouter = require('../routes/login');
 const registerRouter = require('../routes/register');
 const productsRouter = require('../routes/products');
+const adminRouter = require('../routes/admin');
 const errorMidleware = require('../middlewares/error');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/customer/products', productsRouter);
+app.use('/admin/manage', adminRouter);
 app.use(errorMidleware);
 
 module.exports = app;
