106dadcd649bef9edd88b65f10ca2d7651a766cd
Valdeci97 <moura97.valdeci@gmail.com>
2022-06-02 15:41:17 -0300

    NEW: Create user model tests

---
+++ back-end/src/tests/models/mocks/userMock.js
@@ -0,0 +1,9 @@
+const validUser = {
+  id: 1,
+  name: 'Delivery App Admin',
+  email: 'adm@deliveryapp.com',
+  password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+  role: 'administrator',
+};
+
+module.exports = { validUser };
