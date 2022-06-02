96556b636f1395e873e31dd5d1ef3ac774c2dc0e
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-02 18:02:04 -0300

    FEAT: getAllProducts route implemented

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const loginRouter = require('../routes/login');
 const registerRouter = require('../routes/register');
+const productsRouter = require('../routes/products');
 const errorMidleware = require('../middlewares/error');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/customer/products', productsRouter);
 app.use(errorMidleware);
 
 module.exports = app;
