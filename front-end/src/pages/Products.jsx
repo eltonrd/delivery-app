6cebb1812b2c4fecb82fc5a31f18b81ff6df1435
Braien Machado de Paula <braienmp@outlook.com>
2022-06-03 18:05:53 -0300

    feat: basic navbar in products page

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocalStorage } from '../utils/localStorage/localStorage';
 
 export default function Products() {
+  const [user, setUser] = useLocalStorage('user', 'Convidado');
+  
   return (
-    <div>Products</div>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li data-testid="customer_products__element-navbar-link-products">
+              Produtos
+            </li>
+            <li data-testid="customer_products__element-navbar-link-orders">
+              Pedidos
+            </li>
+          <li data-testid="customer_products__element-navbar-user-full-name">
+              {user.name}
+            </li>
+            <li data-testid="customer_products__element-navbar-link-logout">
+              Logout
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
+
+//
