edd60f05f0ff6ad641f3d90dde1f6c31994f34e4
Braien Machado de Paula <braienmp@outlook.com>
2022-06-03 19:25:14 -0300

    fix: set images' width

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ export default function ProductCard({ product }) {
   const {id, name, urlImage, price } = product;
   return (
     <div>
-      <img src={urlImage} alt={name} data-testid={`customer_products__img-card-bg-image-${id}`} />
+      <img src={urlImage} width={50} alt={name} data-testid={`customer_products__img-card-bg-image-${id}`} />
       <span data-testid={`customer_products__element-card-title-${id}`}>{name}</span>
       <span data-testid={`customer_products__element-card-price-${id}`}>{`R$ ${price}`}</span>
       <button type="button" data-testid={`customer_products__button-card-rm-item-${id}`}>-</button>
