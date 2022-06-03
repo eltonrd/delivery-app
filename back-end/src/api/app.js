a82668982f504915af3b68011129ed1f8ea7f1b5
Valdeci97 <moura97.valdeci@gmail.com>
2022-06-02 22:24:55 -0300

    NEW: Add sales route at server app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,12 +4,14 @@ const cors = require('cors');
 const loginRouter = require('../routes/login');
 const registerRouter = require('../routes/register');
 const errorMidleware = require('../middlewares/error');
+const salesRouter = require('../routes/sales');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/sales', salesRouter);
 app.use(errorMidleware);
 
 module.exports = app;
