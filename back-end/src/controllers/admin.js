c07c844476e9e2cbbfe87966f187a94248dae85f
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-03 15:06:58 -0300

    REFACTOR: admin and product controllers Co-authored-by: Isadora Bayma <isadorabayma@users.noreply.github.com>

--- back-end/src/controllers/admin.js
+++ back-end/src/controllers/admin.js
@@ -3,8 +3,7 @@ const db = require('../services/admin');
 const getAllUsers = async (_req, res, next) => {
   try {
     const allUsers = await db.getAllUsers();
-    if (!allUsers) return res.status(404).json({ message: 'Users not found' });
-    return res.status(201).json(allUsers);
+    return res.status(200).json(allUsers);
   } catch (err) {
     next(err);
   }
--- back-end/src/controllers/products.js
+++ back-end/src/controllers/products.js
@@ -3,8 +3,7 @@ const db = require('../services/products');
 const getAllProducts = async (_req, res, next) => {
   try {
     const allProducts = await db.getAllProducts();
-    if (!allProducts) return res.status(404).json({ message: 'Products not found' });
-    return res.status(201).json(allProducts);
+    return res.status(200).json(allProducts);
   } catch (err) {
     next(err);
   }
