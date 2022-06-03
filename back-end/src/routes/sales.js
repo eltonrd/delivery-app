7e08fbd01895c0c0e7a205a1f557f41eb74bacdf
Braien Machado de Paula <braienmp@outlook.com>
2022-06-03 10:49:23 -0300

    feat: sale validation middleware

--- back-end/src/routes/sales.js
+++ back-end/src/routes/sales.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 
 const { createSale } = require('../controllers/sales');
 const getUser = require('../middlewares/getUser');
+const { saleMiddleware, saleProductsMiddleware } = require('../middlewares/sale');
 
 const salesRouter = Router();
 
-salesRouter.post('/', getUser, createSale);
+salesRouter.post('/', getUser, saleMiddleware, saleProductsMiddleware, createSale);
 
 module.exports = salesRouter;
