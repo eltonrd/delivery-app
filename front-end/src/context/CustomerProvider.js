7d0f8eedd19337033def2450edb96064f8924c76
Braien Machado de Paula <braienmp@outlook.com>
2022-06-07 15:40:17 -0300

    feat: save cart with every change

--- front-end/src/context/CustomerProvider.js
+++ front-end/src/context/CustomerProvider.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import CustomerContext from './CustomerContext';
+import { localStorageCart, setLocalStorageCart } from '../utils/localStorage/localStorage';
 
 function CustomerProvider({ children }) {
   const [cart, setCart] = useState([]);
@@ -9,6 +10,16 @@ function CustomerProvider({ children }) {
     cart,
     setCart,
   };
+
+  useEffect(() => {
+    const localCart = localStorageCart();
+
+    if (cart.length === 0 && localCart.length !== 0) {
+      setCart(localCart);
+    } else {
+      setLocalStorageCart(cart);
+    }
+  }, [cart]);
 
   return (
     <CustomerContext.Provider value={ contextValue }>
