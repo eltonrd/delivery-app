240b8becd3dfaf314ee44eb23461650e564e40eb
Braien Machado de Paula <87545637+braien-machado@users.noreply.github.com>
2022-06-07 17:43:16 -0300

    Update AdminManage.jsx

--- front-end/src/pages/AdminManage.jsx
+++ front-end/src/pages/AdminManage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar';
 // import UserList from '../components/UserList';
-import { addRegister } from '../utils/api/service';
+import { adminRegister } from '../utils/api/service';
 
 export default function AdminManage() {
   const [name, setName] = useState('');
