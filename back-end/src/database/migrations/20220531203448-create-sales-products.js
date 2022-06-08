1c1f269dc11e5b0cf34f3c74ab3da528e64e26dc
Braien Machado de Paula <braienmp@outlook.com>
2022-06-08 16:35:17 -0300

    fix: tablename

--- back-end/src/database/migrations/20220531203448-create-sales-products.js
+++ back-end/src/database/migrations/20220531203448-create-sales-products.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('salesProducts', {
+    await queryInterface.createTable('sales_products', {
       saleId: {
         type: Sequelize.INTEGER,
         primaryKey: true,
