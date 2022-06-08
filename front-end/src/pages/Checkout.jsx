e3f0b62e3cc6e824e5c929ea6aa7441774597ca5
Braien Machado de Paula <braienmp@outlook.com>
2022-06-08 18:56:14 -0300

    feat: Checkout page

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import TableRow from '../components/TableRow';
+import { localStorageCart } from '../utils/localStorage/localStorage';
+import AdressForm from '../components/AdressForm';
+import priceToReal from '../utils/helpers/priceToReal';
 
 export default function Checkout() {
+  const cart = localStorageCart();
+
+  const getTotalPrice = () => {
+    const price = cart
+      .reduce(
+        (acc, cur) => acc + (parseFloat(cur.price) * parseFloat(cur.qty)),
+        0,
+      );
+    return priceToReal(price);
+  };
+
+  const tableHead = () => (
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Descrição</th>
+        <th>Quantidade</th>
+        <th>Valor Unitário</th>
+        <th>Sub-total</th>
+        <th>Remover Item</th>
+      </tr>
+    </thead>
+  );
+
   return (
     <div>
       <NavBar />
-      <h1>Checkout</h1>
+      <section>
+        <h1>Finalizar Pedido</h1>
+        <table>
+          { tableHead() }
+          <tbody>
+            { cart.map((product, index) => (
+              <TableRow
+                product={ product }
+                index={ index }
+                key={ index }
+              />
+            )) }
+          </tbody>
+        </table>
+        <h1>
+          Total: R$
+          <span
+            data-testid="customer_checkout__element-order-total-price"
+          >
+            { getTotalPrice() }
+          </span>
+        </h1>
+      </section>
+      <AdressForm />
     </div>
   );
 }
