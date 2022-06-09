ec99bf853973623ee067548ab488c3a157646ce5
Braien Machado de Paula <braienmp@outlook.com>
2022-06-09 09:05:00 -0300

    feat: useLocalStorage hook

--- front-end/src/utils/localStorage/localStorage.js
+++ front-end/src/utils/localStorage/localStorage.js
@@ -1,3 +1,5 @@
+const { useState } = require('react');
+
 function localStorageUser() {
   const user = JSON.parse(localStorage.getItem('user'));
 
@@ -22,10 +24,46 @@ function removeFromLocalStorage(key) {
   localStorage.removeItem(key);
 }
 
+// useLocalStorage hook explanation: https://usehooks.com/useLocalStorage/
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+
+    try {
+      const item = window.localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function
+        ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
 module.exports = {
   localStorageUser,
   setLocalStorageUser,
   localStorageCart,
   setLocalStorageCart,
   removeFromLocalStorage,
+  useLocalStorage,
 };
