b0e9fef64fd33ca1c81c2afe239547ca48eae2fd
Braien Machado de Paula <braienmp@outlook.com>
2022-06-10 15:44:46 -0300

    fix: token getter

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-// import { localStorageUser } from '../utils/localStorage/localStorage';
+import { localStorageUser } from '../utils/localStorage/localStorage';
 import { getSellerOrders } from '../utils/api/service';
 import NavBar from '../components/NavBar';
 import SellerOrderCard from '../components/SellerOrderCard';
 
 export default function SellerOrders() {
   const [orders, setOrders] = useState([]);
-  // console.log(localStorageUser);
 
   useEffect(() => {
     const getOrders = async () => {
-      const { token } = localStorage.getItem('user');
+      const { token } = localStorageUser();
       const sellerOrders = await getSellerOrders(token);
 
       if (Array.isArray(sellerOrders) && sellerOrders.length > 0) {
