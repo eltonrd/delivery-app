8a459b58f52e632fa0b5172dba702214ff4ce25a
Valdeci97 <moura97.valdeci@gmail.com>
2022-06-14 11:14:52 -0300

    NEW: Create render with router function

---
+++ front-end/src/tests/renderWithRouter/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+export default function renderWithRouter(component) {
+  const history = createMemoryHistory();
+  return ({
+    ...render(
+      <Router location={ history.location } navigator={ history }>
+        { component }
+      </Router>,
+    ),
+  });
+}
