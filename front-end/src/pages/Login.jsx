8e4acd1a554b73daa777293c77997f67c530dd10
valdeci <moura97.valdeci@gmail.com>
2022-07-13 22:12:10 -0300

    feat: add login css style

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,35 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, Navigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import { login } from '../utils/api/service';
-import RockGlass from '../images/rockGlass.svg';
 import {
   setLocalStorageUser,
   localStorageUser,
 } from '../utils/localStorage/localStorage';
 import * as S from '../styles/login';
 
-import '../styles/login.css';
-
 export default function Login() {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [isDisabled, setIsDisabled] = useState(true);
-  const [reveal, setReveal] = useState(false);
+  // const [reveal, setReveal] = useState(false);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const validateInputs = () => {
-      const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+\.?[a-z]+$/;
-      const minPasswordLength = 8;
-
-      setIsDisabled(password.length < minPasswordLength || !regex.test(email));
-    };
-
-    validateInputs();
-  }, [password, email]);
 
   const handleRole = (role) => {
     switch (role) {
@@ -56,61 +41,61 @@ export default function Login() {
 
   if (!localStorageUser()) {
     return (
-      <S.Container className="login-container">
-        <img alt="logo app delivery" src={ RockGlass } width="100px" id="logo" />
-        <h1>Delivery App</h1>
-        <S.Form className="form-container">
-          <label htmlFor="login-input" className="login-label">
-            Login
-            <input
-              data-testid="common_login__input-email"
-              id="login-input"
-              name="login"
-              onChange={ ({ target }) => setEmail(target.value) }
-              type="text"
-              placeholder="customer@deliveryapp.com"
-            />
-          </label>
-          <label htmlFor="password-input" className="password-label">
-            Senha
-            <input
-              data-testid="common_login__input-password"
-              id="password-input"
-              name="password"
-              onChange={ ({ target }) => setPassword(target.value) }
-              type={ reveal ? 'text' : 'password' }
-              placeholder="********"
-            />
-            <button
-              type="button"
-              className="password-handler"
-              onClick={ () => setReveal(!reveal) }
-            >
-              {
+      <>
+        <header>Eu sou o header</header>
+        <S.Container>
+          <S.Form>
+            <S.Label htmlFor="login-input">
+              Email
+              <S.Input
+                data-testid="common_login__input-email"
+                id="login-input"
+                name="login"
+                onChange={ ({ target }) => setEmail(target.value) }
+                type="text"
+                placeholder="customer@deliveryapp.com"
+              />
+            </S.Label>
+            <S.Label htmlFor="password-input">
+              Senha
+              <S.Input
+                data-testid="common_login__input-password"
+                id="password-input"
+                name="password"
+                onChange={ ({ target }) => setPassword(target.value) }
+                // type={ reveal ? 'text' : 'password' }
+                type="password"
+                placeholder="********"
+                // endAdorment={}
+              />
+              {/* <button
+                type="button"
+                onClick={ () => setReveal(!reveal) }
+              >
+                {
                 reveal ? <img src="https://img.icons8.com/ios/50/000000/visible--v1.png" alt="revelar senha" />
                   : <img src="https://img.icons8.com/ios/50/000000/hide.png" alt="ocultar senha" />
               }
-            </button>
-          </label>
-          <button
-            className={ isDisabled ? 'login-button-disabled' : 'login-button' }
-            type="button"
-            data-testid="common_login__button-login"
-            disabled={ isDisabled }
-            onClick={ sendLoginInfo }
-          >
-            Login
-          </button>
-          <button
-            className="sign-up"
-            type="button"
-            data-testid="common_login__button-register"
-            onClick={ () => navigate('/register') }
-          >
-            Não tenho conta
-          </button>
-        </S.Form>
-      </S.Container>
+              </button> */}
+            </S.Label>
+            <S.LoginButton
+              type="button"
+              data-testid="common_login__button-login"
+              onClick={ sendLoginInfo }
+            >
+              Login
+            </S.LoginButton>
+            <S.SignupButton
+              type="button"
+              data-testid="common_login__button-register"
+              onClick={ () => navigate('/register') }
+            >
+              Não tenho conta
+            </S.SignupButton>
+          </S.Form>
+        </S.Container>
+        <footer>Eu sou o footer</footer>
+      </>
     );
   }
 
--- front-end/src/styles/login.js
+++ front-end/src/styles/login.js
@@ -1,9 +1,75 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  height: 90vh;
+  justify-content: center;
+  width: 100%;
 `;
 
 export const Form = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 60vh;
+`;
 
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  font-size: large;
+  font-weight: bold;
+  margin-bottom: 5px;
+  text-align: center;
+`;
+
+export const Input = styled.input`
+  border: solid 1px black;
+  border-radius: 4px;
+  height: 30px;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const LoginButton = styled.button`
+  background: #ada8b6;
+  border: 1px solid black;
+  border-radius: 8px;
+  color: #000;
+  font-size: 15px;
+  font-weight: bold;
+  height: 30px;
+  margin: 8px 0;
+  width: 150px;
+
+  &:hover {
+    background: #23022e;
+    color: #fff;
+    cursor: pointer;
+    font-size: 20px;
+    height: 40px;
+    transition: all 0.5s ease-in;
+    width: 200px;
+  }
+`;
+
+export const SignupButton = styled.button`
+  background: #23022e;
+  border: none;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 15px;
+  font-weight: bold;
+  height: 30px;
+  width: 150px;
+
+  &:hover {
+    cursor: pointer;
+    font-size: 20px;
+    height: 40px;
+    transition: all 0.5s ease-in;
+    width: 200px;
+  }
 `;
