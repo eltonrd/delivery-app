5e88d0f150fb7993e7ee98d57f758ae81b09f790
valdeci <moura97.valdeci@gmail.com>
2022-07-14 23:14:41 -0300

    feat: add responsive style to header title

--- front-end/src/styles/loginHeader.js
+++ front-end/src/styles/loginHeader.js
@@ -10,7 +10,21 @@ export const Container = styled.header`
 `;
 
 export const Title = styled.span`
+  @media(max-width: 400px) {
+    font-size: 20px;
+  }
 
+  @media(min-width: 401px) and (max-width: 440px) {
+    font-size: 22px;
+  }
+
+  @media(min-width: 441px) and (max-width: 540px) {
+    font-size: 25px;
+  }
+
+  @media(min-width: 541px) and (max-width: 840px) {
+    font-size: 28px;
+  }
 `;
 
 export const Theme = styled.label`
