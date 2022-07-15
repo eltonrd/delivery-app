24b450489fb3326fd737cf7bf9fa1e796b053dd6
valdeci <moura97.valdeci@gmail.com>
2022-07-14 23:03:52 -0300

    feat: create util function to help with github links

---
+++ front-end/src/utils/social/index.js
@@ -0,0 +1,24 @@
+const githubLink = [
+  {
+    src: 'https://github.com/braien-machado',
+    name: 'Braien Machado',
+  },
+  {
+    src: 'https://github.com/eltonrd',
+    name: 'Elton Rodrigues',
+  },
+  {
+    src: 'https://github.com/isadorabayma',
+    name: 'Isador Bayma',
+  },
+  {
+    src: 'https://github.com/Valdeci97',
+    name: 'Valdeci de Moura',
+  },
+  {
+    src: 'https://github.com/wltonlopes',
+    name: 'Welton Lopes',
+  },
+];
+
+export default githubLink;
