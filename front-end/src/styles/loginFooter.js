7ad0d40669f4219a88121a77170e6a8699e3a236
valdeci <moura97.valdeci@gmail.com>
2022-07-14 23:03:23 -0300

    feat: create footer style

---
+++ front-end/src/styles/loginFooter.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  align-items: center;
+  bottom: 0;
+  display: flex;
+  font-size: 20px;
+  height: 84px;
+  justify-content: space-around;
+  border-top: solid 1px black;
+  width: 100%;
+
+  a {
+    color: black;
+    font-weight: bold;
+    text-decoration: none;
+  }
+`;
+
+export default Footer;
