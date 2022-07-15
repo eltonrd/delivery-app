3a16f9abee4597da79c3a6b0408a5cee27b0150c
valdeci <moura97.valdeci@gmail.com>
2022-07-15 11:43:17 -0300

    feat: Aplly styles update, remove disabled button behaviour and customize toastify message

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5';
 import { register } from '../utils/api/service';
 import { setLocalStorageUser } from '../utils/localStorage/localStorage';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import * as S from '../styles/register';
+import ToastMessage from '../utils/helpers/toastifyMessage';
 
-import '../styles/register.css';
 import 'react-toastify/dist/ReactToastify.css';
+
+const TOASTIFY_POSITION = 'top-center';
 
 export default function Register() {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-  const [isDisabled, setIsDisabled] = useState(true);
   const [confirmPassword, setConfirmPassword] = useState('');
   const [reveal, setReveal] = useState(false);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    toast.success(`
-      Para ativar o botão de cadastro:
-      Preencha todos os campos, a senha deve conter ao menos uma letra maiúscula,
-      uma minúscula, no mínimo 8 e no máximo 64 caracteres de comprimento e um caractere
-      especial. ( $*&@# )
-    `);
-  }, []);
+  const resetState = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  };
 
-  useEffect(() => {
-    const validateInputs = () => {
-      const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+\.?[a-z]+$/;
-      const PASSWORD_BODY = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])/;
-      const PASSWORD_LENGTH = /(?:([0-9a-zA-Z$*&@#])(?!\1)){8,64}/;
-      const passwordRegex = new RegExp(PASSWORD_BODY.source + PASSWORD_LENGTH.source);
-      const minNameLength = 12;
-
-      setIsDisabled(
-        !passwordRegex.test(password) || !passwordRegex.test(confirmPassword)
-        || !regex.test(email)
-        || name.length < minNameLength,
-      );
-    };
-
-    validateInputs();
-  }, [password, email, name, confirmPassword]);
+  const validatePassword = (pass, confirm) => {
+    const PASSWORD_BODY = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])/;
+    const PASSWORD_LENGTH = /(?:([0-9a-zA-Z$*&@#])(?!\1)){8,64}/;
+    const regex = new RegExp(PASSWORD_BODY.source + PASSWORD_LENGTH.source);
+    if (!regex.test(pass) || !regex.test(confirm)) {
+      return false;
+    }
+    return true;
+  };
 
   const sendRegisterInfo = async () => {
     if (confirmPassword !== password) {
-      return toast.warning('As senhas tem que ser iguais.');
+      return toast.warning('As senhas informadas não são iguais.');
+    }
+
+    if (!validatePassword(password, confirmPassword)) {
+      return toast.warning(<ToastMessage />,
+        { theme: 'dark', position: TOASTIFY_POSITION });
     }
 
     const TWO_SECONDS = 2000;
@@ -54,21 +54,25 @@ export default function Register() {
     const userInfo = await register(name, email, password);
 
     if (!userInfo) {
-      return toast.error('Usuário já cadastrado!');
+      return toast.error('Usuário já cadastrado!',
+        { theme: 'dark', position: TOASTIFY_POSITION });
     }
 
-    toast.success('Cadastro efetuado com sucesso!');
+    toast.success('Cadastro efetuado com sucesso!',
+      { theme: 'dark', position: TOASTIFY_POSITION });
+    resetState();
     const { user, token } = userInfo;
     setLocalStorageUser({ ...user, token });
     setTimeout(() => navigate('/customer/products'), TWO_SECONDS);
   };
 
   return (
-    <div>
-      <form className="register-container">
-        <label htmlFor="name-input" className="label-input">
+    <>
+      <Header />
+      <S.Container>
+        <S.Label htmlFor="name-input">
           Nome
-          <input
+          <S.Input
             data-testid="common_register__input-name"
             id="name-input"
             onChange={ ({ target }) => setName(target.value) }
@@ -77,10 +81,10 @@ export default function Register() {
             placeholder="Seu nome"
             value={ name }
           />
-        </label>
-        <label htmlFor="email-input" className="label-input">
+        </S.Label>
+        <S.Label htmlFor="email-input">
           Email
-          <input
+          <S.Input
             data-testid="common_register__input-email"
             id="email-input"
             onChange={ ({ target }) => setEmail(target.value) }
@@ -89,10 +93,10 @@ export default function Register() {
             value={ email }
             placeholder="customer@deliveryapp.com"
           />
-        </label>
-        <label htmlFor="password-input" className="label-input">
+        </S.Label>
+        <S.Label htmlFor="password-input">
           Senha
-          <input
+          <S.Input
             data-testid="common_register__input-password"
             id="password-input"
             onChange={ ({ target }) => setPassword(target.value) }
@@ -101,20 +105,18 @@ export default function Register() {
             value={ password }
             placeholder="********"
           />
-          <button
+          <S.RevealPassword
             type="button"
-            className="password-button"
-            onClick={ (() => setReveal(!reveal)) }
+            onClick={ () => setReveal(!reveal) }
           >
             {
-              reveal ? <img src="https://img.icons8.com/ios/50/000000/visible--v1.png" alt="revelar senha" />
-                : <img src="https://img.icons8.com/ios/50/000000/hide.png" alt="ocultar senha" />
+              reveal ? <IoEyeOutline /> : <IoEyeOffOutline />
             }
-          </button>
-        </label>
-        <label htmlFor="confirm-password-input" className="label-input">
+          </S.RevealPassword>
+        </S.Label>
+        <S.Label htmlFor="confirm-password-input">
           Confirmar Senha
-          <input
+          <S.Input
             id="confirm-password-input"
             type={ reveal ? 'text' : 'password' }
             value={ confirmPassword }
@@ -122,17 +124,24 @@ export default function Register() {
             name="confirm-password"
             placeholder="********"
           />
-        </label>
-        <button
-          className={ isDisabled ? 'create-button-disabled' : 'create-button' }
+          <S.RevealPassword
+            type="button"
+            onClick={ () => setReveal(!reveal) }
+          >
+            {
+              reveal ? <IoEyeOutline /> : <IoEyeOffOutline />
+            }
+          </S.RevealPassword>
+        </S.Label>
+        <S.Button
           data-testid="common_register__button-register"
-          disabled={ isDisabled }
           type="button"
           onClick={ sendRegisterInfo }
         >
           Cadastrar
-        </button>
-      </form>
-    </div>
+        </S.Button>
+      </S.Container>
+      <Footer />
+    </>
   );
 }
