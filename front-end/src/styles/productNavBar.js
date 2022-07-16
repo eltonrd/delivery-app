20feaf5226a8e2d5f4e2af93657995e29b1bcb15
valdeci <moura97.valdeci@gmail.com>
2022-07-16 12:00:15 -0300

    feat: add responsive style on product nav bar component

--- front-end/src/styles/productNavBar.js
+++ front-end/src/styles/productNavBar.js
@@ -21,6 +21,34 @@ export const List = styled.ul`
     margin: 0 2.5rem;
     text-decoration: none;
   }
+
+  @media(max-width: 440px) {
+    a {
+      font-size: 15px;
+      margin: 0 0.5rem;
+    }
+  }
+
+  @media(min-width: 441px) and (max-width: 540px) {
+    a {
+      font-size: 18px;
+      margin: 0 0.75rem;
+    }
+  }
+
+  @media(min-width: 541px) and (max-width: 720px) {
+    a {
+      font-size: 20px;
+      margin: 0 1rem;
+    }
+  }
+
+  @media(min-width: 721px) and (max-width: 1080px) {
+    a {
+      font-size: 22px;
+      margin: 0 1.2rem;
+    }
+  }
 `;
 
 export const RightSide = styled.div`
@@ -32,16 +60,57 @@ export const Title = styled.h1`
   font-size: 25px;
   font-weight: bold;
   margin-right: 30px;
+
+  @media(max-width: 720px) {
+    display: none;
+  }
+
+  @media(min-width: 721px) and (max-width: 1080px) {
+    font-size: 22px;
+  }
 `;
 
 export const Logout = styled.button`
+  @media(max-width: 720px) {
+    margin-right: 15px;
+  }
+
+  @media(max-width: 440px) {
+    font-size: 15px;
+    height: 25px;
+    width: 80px;
+  }
+
+  @media(min-width: 441px) and (max-width: 540px) {
+    font-size: 18px;
+    height: 25px;
+    width: 100px;
+  }
+
+  @media(min-width: 541px) and (max-width: 720px) {
+    font-size: 20px;
+    height: 28px;
+    width: 120px;
+  }
+
+  @media(min-width: 721px) and (max-width: 1080px) {
+    font-size: 22px;
+    height: 28px;
+    margin-left: 1.2rem;
+    margin-right: 1.2rem;
+    width: 120px;
+  }
+
+  @media(min-width: 1081px) {
+    margin-left: 1.4rem;
+  }
+
   background: #23022e;
   border: none;
   border-radius: 8px;
   color: #fff;
   font-size: 20px;
   height: 30px;
-  margin-left: 30px;
   width: 150px;
 
   &:hover {
